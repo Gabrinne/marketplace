5c534458caaaf1093748702ca1f459c32ccf1cbe
Gabrinne <gabrinneloyse@hotmail.com>
2020-05-16 00:01:10 -0300

    validando lojas

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -5,28 +5,48 @@
 <div class="container">
 
     <h1>Criar loja</h1>
-    <form action="{{route('admin.stores.store')}}" method="POST">
-        @csrf
+<form action="{{route('admin.stores.store')}}" method="POST">
+    @csrf
         
         
-        <div class="form-group">
-            <label>Nome loja</label>
-            <input type="text" name="name" class="form-control">
+<div class="form-group">
+    <label>Nome loja</label>
+    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}" >
+        @error('name')
+        <div class="invalid-feedback">
+            {{$message}}
         </div>
+      @enderror
+    </div>
 
-        <div class="form-group">
-            <label>Descrição</label>
-    <input type="text" name="description" class="form-control">
+<div class="form-group">
+        <label>Descrição</label>
+    <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}" >
+    @error('description')
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+    @enderror
 </div>
 
 <div class="form-group">
     <label>Telefone</label>
-    <input type="text" name="phone" class="form-control">
+    <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}" >
+    @error('phone')
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+    @enderror
 </div>
 
 <div class="form-group">
     <label>Celular</label>
-    <input type="text" name="mobile_phone" class="form-control">
+    <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror" value="{{old('mobile_phone')}}" >
+    @error('mobile_phone')
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+    @enderror
 </div>
 
 <div class="form-group">
